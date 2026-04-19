cce28515fe359c523ad68db4645181a2af705626
LukeisStuff <lukeisawesome03@gmail.com>
2026-04-18 22:12:15 -0400

    slate, permafrost, netherrack and gloomstone statues

--- src/main/java/betterthandeer/btd/item/BTDItems.java
+++ src/main/java/betterthandeer/btd/item/BTDItems.java
@@ -3,6 +3,7 @@ package betterthandeer.btd.item;
 import betterthandeer.btd.block.BTDBlocks;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemBucket;
+import net.minecraft.core.item.ItemStatue;
 import net.minecraft.core.item.Items;
 
 public class BTDItems {
@@ -20,6 +21,11 @@ public class BTDItems {
 	public static Item SULFUR;
 
 	public static Item AMMO_ARROW_FLAMING;
+
+	public static Item STATUE_SLATE;
+	public static Item STATUE_PERMAFROST;
+	public static Item STATUE_NETHERRACK;
+	public static Item STATUE_GLOOMSTONE;
 
 	private static boolean hasInit = false;
 
@@ -44,5 +50,10 @@ public class BTDItems {
 		SULFUR = new Item("sulfur", "btd:item/sulfur", 20005);
 
 		AMMO_ARROW_FLAMING = new Item("ammo.arrow.flaming", "btd:item/ammo_arrow_flaming", 20006);
+
+		STATUE_SLATE = new ItemStatue("statue.slate", "btd:item/statue_slate", 20050, BTDBlocks.STATUE_SLATE_LOWER, BTDBlocks.STATUE_SLATE_UPPER);
+		STATUE_PERMAFROST = new ItemStatue("statue.permafrost", "btd:item/statue_permafrost", 20051, BTDBlocks.STATUE_PERMAFROST_LOWER, BTDBlocks.STATUE_PERMAFROST_UPPER);
+		STATUE_NETHERRACK = new ItemStatue("statue.netherrack", "btd:item/statue_netherrack", 20052, BTDBlocks.STATUE_NETHERRACK_LOWER, BTDBlocks.STATUE_NETHERRACK_UPPER);
+		STATUE_GLOOMSTONE = new ItemStatue("statue.gloomstone", "btd:item/statue_gloomstone", 20053, BTDBlocks.STATUE_GLOOMSTONE_LOWER, BTDBlocks.STATUE_GLOOMSTONE_UPPER);
 	}
 }
